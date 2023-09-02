2d9710d352c097ce9957c7e7fe7d4c61610e0163
Reoseah <reoseah9@gmail.com>
2023-09-02 03:47:04 +0200

    assets: laser, warp crystal

--- src/main/java/com/spacefactory/SpaceFactory.java
+++ src/main/java/com/spacefactory/SpaceFactory.java
@@ -14,6 +14,8 @@ import org.slf4j.LoggerFactory;
 public class SpaceFactory {
     public static final Logger LOGGER = LoggerFactory.getLogger("spacefactory");
 
+    public static final Item COAL_DUST = new Item(new Item.Settings());
+    public static final Item ENDER_PEARL_DUST = new Item(new Item.Settings());
     public static final Item ULTRAPURE_IRON = new Item(new Item.Settings());
     public static final Item ULTRAPURE_COPPER = new Item(new Item.Settings());
     public static final Item ULTRAPURE_GOLD = new Item(new Item.Settings());
@@ -25,10 +27,14 @@ public class SpaceFactory {
     public static final Item ELECTRIC_MOTOR = new Item(new Item.Settings());
     public static final Item TRANSFORMER = new Item(new Item.Settings());
     public static final Item SUPERCAPACITOR = new Item(new Item.Settings());
+    public static final Item RESONANT_LASER = new Item(new Item.Settings());
+    public static final Item WARP_CRYSTAL = new Item(new Item.Settings());
 
     public static void initialize() {
         LOGGER.info("Initializing...");
 
+        Registry.register(Registries.ITEM, "spacefactory:coal_dust", COAL_DUST);
+        Registry.register(Registries.ITEM, "spacefactory:ender_pearl_dust", ENDER_PEARL_DUST);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_iron", ULTRAPURE_IRON);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_copper", ULTRAPURE_COPPER);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_gold", ULTRAPURE_GOLD);
@@ -40,11 +46,15 @@ public class SpaceFactory {
         Registry.register(Registries.ITEM, "spacefactory:electric_motor", ELECTRIC_MOTOR);
         Registry.register(Registries.ITEM, "spacefactory:transformer", TRANSFORMER);
         Registry.register(Registries.ITEM, "spacefactory:supercapacitor", SUPERCAPACITOR);
+        Registry.register(Registries.ITEM, "spacefactory:resonant_laser", RESONANT_LASER);
+        Registry.register(Registries.ITEM, "spacefactory:warp_crystal", WARP_CRYSTAL);
 
         ItemGroup itemGroup = FabricItemGroup.builder()
                 .displayName(Text.translatable("itemGroup.spacefactory"))
                 .icon(() -> new ItemStack(SpaceFactory.ULTRAPURE_IRON))
                 .entries((displayContext, entries) -> {
+                    entries.add(COAL_DUST);
+                    entries.add(ENDER_PEARL_DUST);
                     entries.add(ULTRAPURE_IRON);
                     entries.add(ULTRAPURE_COPPER);
                     entries.add(ULTRAPURE_GOLD);
@@ -56,6 +66,8 @@ public class SpaceFactory {
                     entries.add(ELECTRIC_MOTOR);
                     entries.add(TRANSFORMER);
                     entries.add(SUPERCAPACITOR);
+                    entries.add(RESONANT_LASER);
+                    entries.add(WARP_CRYSTAL);
                 })
                 .build();
         Registry.register(Registries.ITEM_GROUP, "spacefactory:main", itemGroup);
