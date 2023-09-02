3f08140c8a51b93ab2188c9d2c04fa3564a6fad3
Reoseah <reoseah9@gmail.com>
2023-09-02 03:03:09 +0200

    assets: ultrapure metal

--- src/main/java/com/spacefactory/SpaceFactory.java
+++ src/main/java/com/spacefactory/SpaceFactory.java
@@ -1,12 +1,48 @@
 package com.spacefactory;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SpaceFactory {
     public static final Logger LOGGER = LoggerFactory.getLogger("spacefactory");
 
+    public static final Item ULTRAPURE_IRON = new Item(new Item.Settings());
+    public static final Item ULTRAPURE_COPPER = new Item(new Item.Settings());
+    public static final Item ULTRAPURE_GOLD = new Item(new Item.Settings());
+    public static final Item ULTRAPURE_SILICON = new Item(new Item.Settings());
+    public static final Item ULTRAPURE_IRON_NUGGET = new Item(new Item.Settings());
+    public static final Item ULTRAPURE_COPPER_NUGGET = new Item(new Item.Settings());
+
     public static void initialize() {
         LOGGER.info("Initializing...");
+
+        Registry.register(Registries.ITEM, "spacefactory:ultrapure_iron", ULTRAPURE_IRON);
+        Registry.register(Registries.ITEM, "spacefactory:ultrapure_copper", ULTRAPURE_COPPER);
+        Registry.register(Registries.ITEM, "spacefactory:ultrapure_gold", ULTRAPURE_GOLD);
+        Registry.register(Registries.ITEM, "spacefactory:ultrapure_silicon", ULTRAPURE_SILICON);
+        Registry.register(Registries.ITEM, "spacefactory:ultrapure_iron_nugget", ULTRAPURE_IRON_NUGGET);
+        Registry.register(Registries.ITEM, "spacefactory:ultrapure_copper_nugget", ULTRAPURE_COPPER_NUGGET);
+
+        ItemGroup itemGroup = FabricItemGroup.builder()
+                .displayName(Text.translatable("itemGroup.spacefactory"))
+                .icon(() -> new ItemStack(SpaceFactory.ULTRAPURE_IRON))
+                .entries((displayContext, entries) -> {
+                    entries.add(ULTRAPURE_IRON);
+                    entries.add(ULTRAPURE_COPPER);
+                    entries.add(ULTRAPURE_GOLD);
+                    entries.add(ULTRAPURE_SILICON);
+                    entries.add(ULTRAPURE_IRON_NUGGET);
+                    entries.add(ULTRAPURE_COPPER_NUGGET);
+                })
+                .build();
+        Registry.register(Registries.ITEM_GROUP, "spacefactory:main", itemGroup);
     }
 }
