b431b80f2b17379107e9c24a3953d06a98d6480d
Reoseah <reoseah9@gmail.com>
2023-09-02 12:13:17 +0200

    assets: ultrapure carbon

--- src/main/java/com/spacefactory/SpaceFactory.java
+++ src/main/java/com/spacefactory/SpaceFactory.java
@@ -2,6 +2,7 @@ package com.spacefactory;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -14,12 +15,11 @@ import org.slf4j.LoggerFactory;
 public class SpaceFactory {
     public static final Logger LOGGER = LoggerFactory.getLogger("spacefactory");
 
-    public static final Item COAL_DUST = new Item(new Item.Settings());
-    public static final Item ENDER_PEARL_DUST = new Item(new Item.Settings());
     public static final Item ULTRAPURE_IRON = new Item(new Item.Settings());
     public static final Item ULTRAPURE_COPPER = new Item(new Item.Settings());
     public static final Item ULTRAPURE_GOLD = new Item(new Item.Settings());
     public static final Item ULTRAPURE_SILICON = new Item(new Item.Settings());
+    public static final Item ULTRAPURE_CARBON = new Item(new Item.Settings());
     public static final Item ULTRAPURE_IRON_NUGGET = new Item(new Item.Settings());
     public static final Item ULTRAPURE_COPPER_NUGGET = new Item(new Item.Settings());
     public static final Item STEEL_SHEET = new Item(new Item.Settings());
@@ -33,12 +33,11 @@ public class SpaceFactory {
     public static void initialize() {
         LOGGER.info("Initializing...");
 
-        Registry.register(Registries.ITEM, "spacefactory:coal_dust", COAL_DUST);
-        Registry.register(Registries.ITEM, "spacefactory:ender_pearl_dust", ENDER_PEARL_DUST);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_iron", ULTRAPURE_IRON);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_copper", ULTRAPURE_COPPER);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_gold", ULTRAPURE_GOLD);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_silicon", ULTRAPURE_SILICON);
+        Registry.register(Registries.ITEM, "spacefactory:ultrapure_carbon", ULTRAPURE_CARBON);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_iron_nugget", ULTRAPURE_IRON_NUGGET);
         Registry.register(Registries.ITEM, "spacefactory:ultrapure_copper_nugget", ULTRAPURE_COPPER_NUGGET);
         Registry.register(Registries.ITEM, "spacefactory:steel_sheet", STEEL_SHEET);
@@ -53,12 +52,11 @@ public class SpaceFactory {
                 .displayName(Text.translatable("itemGroup.spacefactory"))
                 .icon(() -> new ItemStack(SpaceFactory.ULTRAPURE_IRON))
                 .entries((displayContext, entries) -> {
-                    entries.add(COAL_DUST);
-                    entries.add(ENDER_PEARL_DUST);
                     entries.add(ULTRAPURE_IRON);
                     entries.add(ULTRAPURE_COPPER);
                     entries.add(ULTRAPURE_GOLD);
                     entries.add(ULTRAPURE_SILICON);
+                    entries.add(ULTRAPURE_CARBON);
                     entries.add(ULTRAPURE_IRON_NUGGET);
                     entries.add(ULTRAPURE_COPPER_NUGGET);
                     entries.add(STEEL_SHEET);
@@ -71,5 +69,7 @@ public class SpaceFactory {
                 })
                 .build();
         Registry.register(Registries.ITEM_GROUP, "spacefactory:main", itemGroup);
+
+        FuelRegistry.INSTANCE.add(ULTRAPURE_CARBON, 8 * 200);
     }
 }
