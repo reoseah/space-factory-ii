2ba48c0c0511b154140eb226917b20f99a263e67
Reoseah <reoseah9@gmail.com>
2023-09-13 01:33:49 +0200

    remove assembler gui experiments

--- src/client/java/com/spacefactory/screen/AssemblerScreen.java
+++ src/client/java/com/spacefactory/screen/AssemblerScreen.java
@@ -14,11 +14,10 @@ public class AssemblerScreen extends HandledScreen<AssemblerScreenHandler> {
     private static final Identifier TEXTURE = new Identifier("spacefactory:textures/gui/smeltery.png");
 
     public AssemblerScreen(AssemblerScreenHandler handler, PlayerInventory inventory, Text title) {
-        super(handler, inventory, ((MutableText) title).formatted(Formatting.AQUA));
+        super(handler, inventory, title);
 
-        this.backgroundWidth = 184;
-        this.backgroundHeight = 198;
-        this.playerInventoryTitleY = this.backgroundHeight - 94 - 2;
+        this.backgroundHeight = 196;
+        this.playerInventoryTitleY = this.backgroundHeight - 94;
     }
 
     @Override
@@ -33,12 +32,6 @@ public class AssemblerScreen extends HandledScreen<AssemblerScreenHandler> {
         super.render(context, mouseX, mouseY, delta);
         this.drawMouseoverTooltip(context, mouseX, mouseY);
     }
-
-    protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
-        context.drawText(this.textRenderer, this.title, this.titleX, this.titleY, 4210752, false);
-        context.drawText(this.textRenderer, Text.translatable("container.inventory").formatted(Formatting.AQUA), this.playerInventoryTitleX, this.playerInventoryTitleY, 4210752, false);
-    }
-
 
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
