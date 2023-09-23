4495b463cd5abe777841f6f6537c07ebddb5f869
Reoseah <reoseah9@gmail.com>
2023-09-23 17:22:22 +0200

    charge launcher mostly working, don't have interest in using it

--- src/main/java/io/github/reoseah/spacefactory/item/ChargeLauncherItem.java
+++ src/main/java/io/github/reoseah/spacefactory/item/ChargeLauncherItem.java
@@ -8,7 +8,10 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.entity.projectile.SmallFireballEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.RangedWeaponItem;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
@@ -22,12 +25,8 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,10 +36,11 @@ public class ChargeLauncherItem extends RangedWeaponItem {
     private static final String CHARGED_KEY = "Charged";
     private static final String CHARGED_PROJECTILES_KEY = "ChargedProjectiles";
     private static final int DEFAULT_PULL_TIME = 25;
-    public static final int RANGE = 8;
+    public static final int RANGE = 16;
+    private static final float DEFAULT_SPEED = 2F;
+
     private boolean charged = false;
     private boolean loaded = false;
-    private static final float DEFAULT_SPEED = 1;
 
     public ChargeLauncherItem(Settings settings) {
         super(settings);
@@ -60,9 +60,9 @@ public class ChargeLauncherItem extends RangedWeaponItem {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack stack = user.getStackInHand(hand);
         if (isCharged(stack)) {
-            shootAll(world, user, hand, stack, getSpeed(stack), 1.0f);
+            shootAll(world, user, hand, stack, getSpeed(stack), 0.0f);
             setCharged(stack, false);
-            return TypedActionResult.consume(stack);
+            return TypedActionResult.success(stack);
         }
         if (!user.getProjectileType(stack).isEmpty()) {
             if (!isCharged(stack)) {
@@ -76,16 +76,13 @@ public class ChargeLauncherItem extends RangedWeaponItem {
     }
 
     private static float getSpeed(ItemStack stack) {
-//        if (hasProjectile(stack, Items.FIRE_CHARGE)) {
-//            return FIREWORK_ROCKET_SPEED;
-//        }
         return DEFAULT_SPEED;
     }
 
     @Override
     public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-        int i = this.getMaxUseTime(stack) - remainingUseTicks;
-        float f = getPullProgress(i, stack);
+        int ticks = this.getMaxUseTime(stack) - remainingUseTicks;
+        float f = getPullProgress(ticks, stack);
         if (f >= 1.0f && !isCharged(stack) && loadProjectiles(user, stack)) {
             setCharged(stack, true);
             SoundCategory soundCategory = user instanceof PlayerEntity ? SoundCategory.PLAYERS : SoundCategory.HOSTILE;
@@ -94,41 +91,27 @@ public class ChargeLauncherItem extends RangedWeaponItem {
     }
 
     private static boolean loadProjectiles(LivingEntity shooter, ItemStack crossbow) {
-        int multishot = EnchantmentHelper.getLevel(Enchantments.MULTISHOT, crossbow);
-        int count = multishot == 0 ? 1 : 3;
-        boolean creativePlayer = shooter instanceof PlayerEntity && ((PlayerEntity) shooter).getAbilities().creativeMode;
+        boolean creativePlayer = shooter instanceof PlayerEntity player //
+                && player.getAbilities().creativeMode;
         ItemStack projectile = shooter.getProjectileType(crossbow);
-        if (shooter instanceof PlayerEntity player && player.getAbilities().creativeMode && projectile.isOf(Items.ARROW)) {
+        if (creativePlayer && (projectile.isOf(Items.ARROW) || projectile.isEmpty())) {
             projectile = new ItemStack(Items.FIRE_CHARGE);
         }
-        ItemStack projectileCopy = projectile.copy();
-        for (int i = 0; i < count; i++) {
-            if (i > 0) {
-                projectile = projectileCopy.copy();
-            }
-            if (projectile.isEmpty() && creativePlayer) {
-                projectile = new ItemStack(Items.ARROW);
-                projectileCopy = projectile.copy();
-            }
-            if (loadProjectile(shooter, crossbow, projectile, i > 0, creativePlayer)) continue;
-            return false;
-        }
-        return true;
+        return loadProjectile(shooter, crossbow, projectile, false, creativePlayer);
     }
 
     private static boolean loadProjectile(LivingEntity shooter, ItemStack crossbow, ItemStack projectile, boolean simulated, boolean creative) {
         if (projectile.isEmpty()) {
             return false;
         }
-        boolean creativePlayerAndNormalArrow = creative && projectile.getItem() instanceof ArrowItem;
         ItemStack stack;
-        if (!(creativePlayerAndNormalArrow || creative || simulated)) {
+        if (creative || simulated) {
+            stack = projectile.copy();
+        } else {
             stack = projectile.split(1);
             if (projectile.isEmpty() && shooter instanceof PlayerEntity) {
                 ((PlayerEntity) shooter).getInventory().removeOne(projectile);
             }
-        } else {
-            stack = projectile.copy();
         }
         putProjectile(crossbow, stack);
         return true;
@@ -157,8 +140,7 @@ public class ChargeLauncherItem extends RangedWeaponItem {
         NbtList nbtList;
         ArrayList<ItemStack> list = Lists.newArrayList();
         NbtCompound nbt = crossbow.getNbt();
-        if (nbt != null && nbt.contains(CHARGED_PROJECTILES_KEY, NbtElement.LIST_TYPE)
-                && (nbtList = nbt.getList(CHARGED_PROJECTILES_KEY, NbtElement.COMPOUND_TYPE)) != null) {
+        if (nbt != null && nbt.contains(CHARGED_PROJECTILES_KEY, NbtElement.LIST_TYPE) && (nbtList = nbt.getList(CHARGED_PROJECTILES_KEY, NbtElement.COMPOUND_TYPE)) != null) {
             for (int i = 0; i < nbtList.size(); ++i) {
                 NbtCompound nbtCompound2 = nbtList.getCompound(i);
                 list.add(ItemStack.fromNbt(nbtCompound2));
@@ -181,60 +163,37 @@ public class ChargeLauncherItem extends RangedWeaponItem {
     }
 
     private static void shoot(World world, LivingEntity shooter, Hand hand, ItemStack crossbow, ItemStack ammo, float soundPitch, boolean creative, float speed, float divergence, float simulated) {
-        ProjectileEntity projectile = null;
+        ProjectileEntity projectile;
         if (world.isClient) {
             return;
         }
-        boolean fireCharge = ammo.isOf(Items.FIRE_CHARGE);
-        if (fireCharge) {
-            projectile = new SmallFireballEntity(world, shooter, 0, 0, 0);
-            projectile.setPos(shooter.getX(), shooter.getEyeY() - (double) 0.15f, shooter.getZ());
+        if (ammo.isOf(Items.FIRE_CHARGE)) {
+            projectile = new SmallFireballEntity(world, shooter, 0, -LivingEntity.GRAVITY, 0);
+            projectile.setNoGravity(false);
+            projectile.setPos(shooter.getX(), shooter.getEyeY() - 0.25f, shooter.getZ());
+        } else {
+            // TODO other types of charges
+            return;
         }
 
-        Vec3d vec3d = shooter.getOppositeRotationVector(1.0f);
-        Quaternionf quaternionf = new Quaternionf().setAngleAxis(simulated * ((float) Math.PI / 180), vec3d.x, vec3d.y, vec3d.z);
-        Vec3d vec3d2 = shooter.getRotationVec(1.0f);
-        Vector3f vector3f = vec3d2.toVector3f().rotate(quaternionf);
-        projectile.setVelocity(vector3f.x(), vector3f.y(), vector3f.z(), speed, divergence);
+        projectile.setVelocity(shooter, shooter.getPitch(1F), shooter.getYaw(1F), 0, speed, divergence);
 
-        crossbow.damage(fireCharge ? 2 : 1, shooter, e -> e.sendToolBreakStatus(hand));
+        crossbow.damage(ammo.isOf(Items.FIRE_CHARGE) ? 2 : 1, shooter, e -> e.sendToolBreakStatus(hand));
         world.spawnEntity(projectile);
         world.playSound(null, shooter.getX(), shooter.getY(), shooter.getZ(), SoundEvents.ITEM_CROSSBOW_SHOOT, SoundCategory.PLAYERS, 1.0f, soundPitch);
     }
 
     public static void shootAll(World world, LivingEntity entity, Hand hand, ItemStack stack, float speed, float divergence) {
         List<ItemStack> projectiles = getProjectiles(stack);
-        float[] soundPitches = getSoundPitches(entity.getRandom());
-        for (int i = 0; i < projectiles.size(); ++i) {
-            boolean creativePlayer;
-            ItemStack ammo = projectiles.get(i);
-            creativePlayer = entity instanceof PlayerEntity
-                    && ((PlayerEntity) entity).getAbilities().creativeMode;
-            if (ammo.isEmpty()) continue;
-            if (i == 0) {
-                shoot(world, entity, hand, stack, ammo, soundPitches[i], creativePlayer, speed, divergence, 0.0f);
-                continue;
-            }
-            if (i == 1) {
-                shoot(world, entity, hand, stack, ammo, soundPitches[i], creativePlayer, speed, divergence, -10.0f);
-                continue;
-            }
-            if (i != 2) {
-                continue;
-            }
-            shoot(world, entity, hand, stack, ammo, soundPitches[i], creativePlayer, speed, divergence, 10.0f);
+        boolean creativePlayer = entity instanceof PlayerEntity //
+                && ((PlayerEntity) entity).getAbilities().creativeMode;
+        ItemStack ammo = projectiles.get(0);
+
+        if (ammo.isEmpty()) {
+            return;
         }
+        shoot(world, entity, hand, stack, ammo, 1, creativePlayer, speed, divergence, 0.0f);
         postShoot(world, entity, stack);
-    }
-
-    private static float[] getSoundPitches(Random random) {
-        boolean flag = random.nextBoolean();
-        return new float[]{1.0f, getSoundPitch(flag, random), getSoundPitch(!flag, random)};
-    }
-
-    private static float getSoundPitch(boolean flag, Random random) {
-        float f = flag ? 0.63f : 0.43f;
-        return 1.0f / (random.nextFloat() * 0.5f + 1.8f) + f;
     }
 
     private static void postShoot(World world, LivingEntity entity, ItemStack stack) {
@@ -250,9 +209,9 @@ public class ChargeLauncherItem extends RangedWeaponItem {
     @Override
     public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
         if (!world.isClient) {
-            int i = EnchantmentHelper.getLevel(Enchantments.QUICK_CHARGE, stack);
-            SoundEvent soundEvent = this.getQuickChargeSound(i);
-            SoundEvent soundEvent2 = i == 0 ? SoundEvents.ITEM_CROSSBOW_LOADING_MIDDLE : null;
+            int quickCharge = EnchantmentHelper.getLevel(Enchantments.QUICK_CHARGE, stack);
+            SoundEvent soundEvent = this.getQuickChargeSound(quickCharge);
+            SoundEvent soundEvent2 = quickCharge == 0 ? SoundEvents.ITEM_CROSSBOW_LOADING_MIDDLE : null;
             float f = (float) (stack.getMaxUseTime() - remainingUseTicks) / (float) getPullTime(stack);
             if (f < 0.2f) {
                 this.charged = false;
@@ -281,30 +240,24 @@ public class ChargeLauncherItem extends RangedWeaponItem {
 
     @Override
     public UseAction getUseAction(ItemStack stack) {
-        return UseAction.CROSSBOW;
+        return UseAction.BOW;
     }
 
     private SoundEvent getQuickChargeSound(int stage) {
-        switch (stage) {
-            case 1: {
-                return SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_1;
-            }
-            case 2: {
-                return SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_2;
-            }
-            case 3: {
-                return SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_3;
-            }
-        }
-        return SoundEvents.ITEM_CROSSBOW_LOADING_START;
+        return switch (stage) {
+            case 1 -> SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_1;
+            case 2 -> SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_2;
+            case 3 -> SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_3;
+            default -> SoundEvents.ITEM_CROSSBOW_LOADING_START;
+        };
     }
 
     private static float getPullProgress(int useTicks, ItemStack stack) {
-        float f = (float) useTicks / (float) getPullTime(stack);
-        if (f > 1.0f) {
-            f = 1.0f;
+        float progress = (float) useTicks / (float) getPullTime(stack);
+        if (progress > 1.0f) {
+            progress = 1.0f;
         }
-        return f;
+        return progress;
     }
 
     @Override
